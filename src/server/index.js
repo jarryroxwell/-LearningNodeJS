7f0fd3745c9004f4757786ecf9ec8bbe152f32d7
jarryroxell <jarry.roxwell@gmail.com>
2020-07-24 15:39:55 +0300

    add pckj Express and junior routing

--- src/server/index.js
+++ src/server/index.js
@@ -2,19 +2,40 @@ var http = require('http'),
     fs = require('fs');
 
 
-
+/* 
+./src/client/index.html
+./src/client/index-red.html
+*/
 
 var server = http.createServer((req, res)=>{
-    console.log(req.url);
-    res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
-    });
+
+    switch (req.url) {
+        case '/favicon.ico':
+            res.end();
+            break
+        case '/':
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            fs.createReadStream('./src/client/index.html').pipe(res);
+            break;
     
-    var mr = fs.createReadStream('./src/client/index.html', 'utf8'); 
-    mr.pipe(res)
-
-
-
+        case '/red':
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            fs.createReadStream('./src/client/index-red.html').pipe(res);
+            break;
+    
+        default:
+            res.writeHead(404, {
+                'Content-Type': 'text/plain; charset=utf-8'
+            });
+            res.end("404");
+            break;
+    }
+//    res.end("Hello World! ||| Привет Мир!");
+//    console.log(req.url);
 });
 
 server.listen(3000, '127.0.0.1');
