6862a7116f56d6e29b0c0d8330e63bec2290f82c
jarryroxell <jarry.roxwell@gmail.com>
2020-07-24 13:28:42 +0300

    create server

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,16 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 
+
+var mr = fs.createReadStream('./.tempfile/text.txt', 'utf8'); 
+var mw = fs.createWriteStream('./.tempfile/text_new.txt'); 
+
+mr.on('data', (chunk)=>{
+    console.log("/n###EW DATA###/n");
+    mw.write(chunk);
+});
+
+/*
 var server = http.createServer((req, res)=>{
     res.writeHead(200, {
         'Content-Type': 'text/plain; charset=utf-8'
@@ -9,3 +20,4 @@ var server = http.createServer((req, res)=>{
 });
 
 server.listen(3000, '127.0.0.1');
+ */
