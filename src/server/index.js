2fbcd2196d8a733b7a5bcb438375ad608d666b59
jarryroxwell <47597593+jarryroxwell@users.noreply.github.com>
2020-07-24 21:37:05 +0300

    fix

--- src/server/index.js
+++ src/server/index.js
@@ -35,7 +35,6 @@ var server = http.createServer((req, res)=>{
             break;
     }
 //    res.end("Hello World! ||| Привет Мир!");
-//    console.log(req.url);
 });
 
 server.listen(3000, '127.0.0.1');
