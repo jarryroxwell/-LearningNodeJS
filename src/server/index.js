48e0ff58ab73290b09214e62897575f552d1014a
jarryroxell <jarry.roxwell@gmail.com>
2020-07-24 12:05:08 +0300

    rmdir and mkdir

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,8 @@
 var fs = require("fs");
 
-var fread = fs.readFileSync(__dirname+"/text.txt", "utf8");
-console.log(fread);
-
-var mes = "hello world";
-var fwrite = fs.writeFileSync("hello_my_world.txt", mes);
-
-fs.readFile(__dirname+"/text.txt", "utf8", (err, data)=>{
-    console.log(data);
+fs.unlink('hello_my_world-2.txt', ()=>{
+    console.log("file deleted");
 });
 
-fs.writeFile("hello_my_world-2.txt", mes, (err, data)=>{
-    console.log('check');
-});
-
-console.log("test");
+/* fs.mkdir("newone", ()=>{}); */
+fs.rmdir("newone", ()=>{});
