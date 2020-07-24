f6b37826cccc17a43739ba559e42857de2705958
jarryroxell <jarry.roxwell@gmail.com>
2020-07-24 13:30:27 +0300

    Stream

--- src/server/index.js
+++ src/server/index.js
@@ -10,14 +10,3 @@ mr.on('data', (chunk)=>{
     mw.write(chunk);
 });
 
-/*
-var server = http.createServer((req, res)=>{
-    res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8'
-    });
-    res.end("Hello World! ||| Привет Мир!");
-    console.log(req.url);
-});
-
-server.listen(3000, '127.0.0.1');
- */
